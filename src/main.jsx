f8aa7e7c4d74ecfe471df2ef49fb5d0d211b8bc3
Im-Pranshu <pranshu.sharma1303@gmail.com>
2024-10-16 20:40:37 +0530

    build fail update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 // Router
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./routes/error_page";
